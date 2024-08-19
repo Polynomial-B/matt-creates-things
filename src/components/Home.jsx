8d9031fac2bbf67a1245029f3e3cb03fd76d8046
Matt <mphlamb@gmail.com>
2024-08-19 17:49:44 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,57 @@
+function Home() {
+	return (
+		<>
+			<header className="hero">
+				<h1>Matt Creates Things</h1>
+			</header>
+			<section>
+				<h2>Skills</h2>
+				<ul>
+                    <li><img src="" alt=""/> HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React.js</li>
+                    <li>TypeScript</li>
+                    <li>Express</li>
+                    <li>Node.js</li>
+                    <li>Python</li>
+                    <li>Django</li>
+                    <li>PostgreSQL</li>
+                    <li>Git/GitHub</li>
+                    <li>MongoDB</li>
+                    <li>Heroku</li>
+                    <li>NPM/PIP</li>
+                </ul>
+			</section>
+            <section>
+				<h2>Currently Learning</h2>
+				<ul>
+                    <li>Next.js</li>
+                </ul>
+			</section>
+            <section>
+				<h2>Currently Working On</h2>
+				<ul>
+                    <li>A basic Typescript synthesiser</li>
+                </ul>
+			</section>
+			<section>
+				<h2>Projects</h2>
+				<p>Paragraph</p>
+			</section>
+			<section>
+				<h2>About Me</h2>
+				<p>Paragraph</p>
+			</section>
+			<section>
+				<h2>Additional Info</h2>
+				<p>Paragraph</p>
+			</section>
+			<footer><a target="_blank" href="">LinkedIn</a> email
+                <br/>App made using React.js, hosted by GitHub
+            </footer>
+		</>
+	);
+}
+
+export default Home;
