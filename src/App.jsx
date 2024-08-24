c6fe4aefa238b1811217b1c5a222fe98e97157d6
Matt <mphlamb@gmail.com>
2024-08-24 15:39:24 +0200

    styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from './components/Home'
 import './App.css'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
     <>
+    <Nav/>
    <Home />
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,25 @@
+import '../App.css'
+
+
 function Home() {
 	return (
 		<>
-			<header className="hero">
-				<h1>Matt Creates Things</h1>
-                <p>I do some stuff</p>
+			<header id="splash">
+				<h1 translate="no">Matt Lamb</h1>
+                <img src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg" alt=""/>
+                <figcaption>&copy; Matt Lamb {new Date().getFullYear()}</figcaption>
+                <p>A full-stack web developer, interested in building creative/innovative/smart/impactful applications.</p>
 			</header>
-			<section>
-				<h2>Skills</h2>
+            <section>
+                <h2>About Me</h2>
+                <p>{"I've always had an interest in functionality. My professional journey has allowed me to engage deeply with the welfare state, espresso, all-weather lifeboats, cartography and linguistics. I love learning and deepening my understanding, especially when I can apply it practically. I'm drawn to Software Engineering for its vast applications and creativity. Most of all, I find it immensely satisfying to build something and understand how it works."}</p>
+                <h2>Skills</h2>
+                Tech Skills
 				<ul>
-                    <li><img src="" alt=""/> HTML</li>
-                    <li>CSS</li>
                     <li>JavaScript</li>
                     <li>React.js</li>
+                    <li><img src="" alt=""/>HTML</li>
+                    <li>CSS</li>
                     <li>TypeScript</li>
                     <li>Express</li>
                     <li>Node.js</li>
@@ -23,38 +31,59 @@ function Home() {
                     <li>Heroku</li>
                     <li>NPM/PIP</li>
                 </ul>
-			</section>
+                Languages
+            </section>
             <section>
 				<h2>Currently Learning...</h2>
 				<ul>
                     <li>Next.js</li>
                 </ul>
 			</section>
+			<section>
+				<h2>Projects</h2>
+				<h3>SynthSounds | 10 Days | Solo Project</h3>
+                <span>GIF, slow enough to see, slow down on hover?</span>
+                <p>React.js, Tone.js, Heroku, Django, PostgreSQL</p>
+                <h3>Shirtify | 7 Days | Paired Project</h3>
+                <p>MongoDB, Express.js, React.js, Node.js</p>
+                <h3>MycoProject | 7 Days | Solo Project</h3>
+                <p>MongoDB, Express.js, Node.js, EJS</p>
+                <h3>Cosmic Ray | 7 Days | Solo Project</h3>
+                <p>JavaScript, HTML and CSS</p>
+			</section>
             <section>
-				<h2>Currently Improving...</h2>
+				<h2>Experience</h2>
 				<ul>
-                    <li>My Typescript skillsr</li>
+                    <li>General Assembly, SEB - 2024</li>
+                    <li>Private English Tutor - dates</li>
+                    <li>Lifeboat Operations Coordinator, RNLI - dates</li>
                 </ul>
 			</section>
 			<section>
-				<h2>Projects</h2>
-				<p>Paragraph</p>
-			</section>
-			<section>
-				<h2>About Me</h2>
-				<p>Paragraph</p>
-			</section>
-			<section>
-				<h2>Additional Info</h2>
-				<p>Paragraph</p>
+				<h2>Interests</h2>
+				<ul>
+                    <li>Sound design - visual elements</li>
+                    <li>Linguistics - visual elements</li>
+                </ul>
                 <h2>Currently...</h2>
                 <ul>
-                    <li>listening to Hania Rani</li>
+                    <li>listening to / current playlist ... </li>
                     <li>reading Dawn by Octavia E. Butler</li>
                 </ul>
 			</section>
-			<footer><a target="_blank" href="">LinkedIn</a> email
-                <br/>App made using React.js and hosted by GitHub pages
+			<footer>
+                <ul>
+                    <li>
+                        <a target="_blank" href="http://www.linkedin.com/in/matt-lamb-dev" rel="noopener noreferrer">LinkedIn</a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="https://github.com/Polynomial-B" rel="noopener noreferrer">GitHub</a>
+                    </li>
+                    <li>
+                        <a target="_blank" href="mailto:m.lamb.dev@gmail.com" rel="noopener noreferrer">Contact</a>
+                    </li>
+                </ul>
+                App made using React.js | 2024
             </footer>
 		</>
 	);
