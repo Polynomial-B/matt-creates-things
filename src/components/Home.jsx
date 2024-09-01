137a3fc1e8325398a2407a18ba0046bfb17b63f2
Matt <mphlamb@gmail.com>
2024-09-01 12:38:40 +0200

    useRef added for scroll behaviour

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,15 @@ import { useRef } from "react";
 import Darklight from "./Darklight";
 
 function Home() {
-	const aboutRef = useRef()
-	
+	const aboutRef = useRef();
+	const projectsRef = useRef();
+	const contactRef = useRef();
+	const homeRef = useRef();
+
 	return (
 		<>
-			<header id="splash">
-		<Darklight/>
+			<header id="splash" ref={homeRef}>
+				{/* <Darklight/> */}
 				<div id="splash-container">
 					<h1 translate="no">MATT LAMB</h1>
 					<h2 className="job-title">SOFTWARE ENGINEER</h2>
@@ -18,17 +21,43 @@ function Home() {
 						alt=""
 						draggable="false"
 					/>
-					<div className="links-container">
-						<button href="#about">about</button>
-						<button href="#projects">projects</button>
-						<button href="#contact">contact</button>
-					</div>
+					<nav className="links-container">
+						<button
+							onClick={() =>
+								aboutRef.current?.scrollIntoView({
+									behavior: "smooth",
+								})
+							}
+						>
+							about
+						</button>
+						<button
+							onClick={() =>
+								projectsRef.current?.scrollIntoView({
+									behavior: "smooth",
+								})
+							}
+						>
+							projects
+						</button>
+						<button
+							onClick={() =>
+								contactRef.current?.scrollIntoView({
+									behavior: "smooth",
+								})
+							}
+						>
+							contact
+						</button>
+					</nav>
 				</div>
 			</header>
 
 			<main>
 				<section>
-					<h2 id="about">About Me</h2>
+					<h2 id="about" ref={aboutRef}>
+						About Me
+					</h2>
 					<p>
 						A full-stack web developer, interested in building
 						creative/innovative/smart/impactful applications.
@@ -52,9 +81,11 @@ function Home() {
 						})}
 					</div>
 					<h2>Learning</h2>
-					<div className="skills-container"><div className="skill-element">Next.js
-					<i className="devicon-nextjs-plain" />
-					</div>
+					<div className="skills-container">
+						<div className="skill-element">
+							Next.js
+							<i className="devicon-nextjs-plain" />
+						</div>
 					</div>
 					<h2>Languages</h2>
 					<ul>
@@ -64,9 +95,11 @@ function Home() {
 					</ul>
 				</section>
 				<section>
-					<h2 id="projects">Projects</h2>
+					<h2 id="projects" ref={projectsRef}>
+						Projects
+					</h2>
 					<div className="project-container">
-						<div className="card">
+						<div className="project-card">
 							<div className="card-image">
 								<img
 									src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg"
@@ -85,7 +118,7 @@ function Home() {
 								</div>
 							</div>
 						</div>
-						<div className="card">
+						<div className="project-card">
 							<div className="card-image">
 								<img
 									src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg"
@@ -103,7 +136,7 @@ function Home() {
 								</div>
 							</div>
 						</div>
-						<div className="card">
+						<div className="project-card">
 							<div className="card-image">
 								<img
 									src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg"
@@ -119,7 +152,7 @@ function Home() {
 								</div>
 							</div>
 						</div>
-						<div className="card">
+						<div className="project-card">
 							<div className="card-image">
 								<img
 									src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg"
@@ -160,49 +193,69 @@ function Home() {
 			</main>
 			<footer>
 				<section className="contact">
-				<h2 id="contact">Contact</h2>
-				<ul>
-					<li className="contact-link">
-						<a
-							target="_blank"
-							href="http://www.linkedin.com/in/matt-lamb-dev"
-							rel="noopener noreferrer"
-						>
-							<figure>
-								<figcaption>LinkedIn</figcaption>
-							<i className="devicon-linkedin-plain"/>
-							</figure>
-						</a>
-					</li>
-					<li className="contact-link">
-						<a
-							target="_blank"
-							href="https://github.com/Polynomial-B"
-							rel="noopener noreferrer"
-						>
-							<figure>
-								<figcaption>GitHub</figcaption>
-								<i className="devicon-github-original" />
-							</figure>
-						</a>
-					</li>
-					<li className="contact-link">
-						<a
-							target="_blank"
-							href="mailto:m.lamb.dev@gmail.com"
-							rel="noopener noreferrer"
-						>
-							<figure>
-								<figcaption>E-mail</figcaption>
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM218 271.7L64.2 172.4C66 156.4 79.5 144 96 144l256 0c16.5 0 30 12.4 31.8 28.4L230 271.7c-1.8 1.2-3.9 1.8-6 1.8s-4.2-.6-6-1.8zm29.4 26.9L384 210.4 384 336c0 17.7-14.3 32-32 32L96 368c-17.7 0-32-14.3-32-32l0-125.6 136.6 88.2c7 4.5 15.1 6.9 23.4 6.9s16.4-2.4 23.4-6.9z"/></svg>
-							</figure>
-						</a>
-					</li>
-				</ul>
+					
+					<h2 id="contact" ref={contactRef}>
+						Contact
+					</h2>
+					<ul>
+						<li className="contact-link">
+							<a
+								target="_blank"
+								href="http://www.linkedin.com/in/matt-lamb-dev"
+								rel="noopener noreferrer"
+							>
+								<figure>
+									<figcaption>LinkedIn</figcaption>
+									<i className="devicon-linkedin-plain" />
+								</figure>
+							</a>
+						</li>
+						<li className="contact-link">
+							<a
+								target="_blank"
+								href="https://github.com/Polynomial-B"
+								rel="noopener noreferrer"
+							>
+								<figure>
+									<figcaption>GitHub</figcaption>
+									<i className="devicon-github-original" />
+								</figure>
+							</a>
+						</li>
+						<li className="contact-link">
+							<a
+								target="_blank"
+								href="mailto:m.lamb.dev@gmail.com"
+								rel="noopener noreferrer"
+							>
+								<figure>
+									<figcaption>E-mail</figcaption>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 448 512"
+									>
+										<path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM218 271.7L64.2 172.4C66 156.4 79.5 144 96 144l256 0c16.5 0 30 12.4 31.8 28.4L230 271.7c-1.8 1.2-3.9 1.8-6 1.8s-4.2-.6-6-1.8zm29.4 26.9L384 210.4 384 336c0 17.7-14.3 32-32 32L96 368c-17.7 0-32-14.3-32-32l0-125.6 136.6 88.2c7 4.5 15.1 6.9 23.4 6.9s16.4-2.4 23.4-6.9z" />
+									</svg>
+								</figure>
+							</a>
+						</li>
+					</ul>
+					<button
+						className="scroll-to-top"
+						onClick={() =>
+							homeRef.current?.scrollIntoView({
+								behavior: "smooth",
+							})
+						}
+					>
+						scroll to top
+					</button>
 				</section>
 				<section className="legal">
-				<div className="footer-email">m.lamb.dev@gmail.com</div>
-				<div className="footer-content">&copy; Matt Lamb {new Date().getFullYear()}</div>
+					<div id="footer-email">m.lamb.dev@gmail.com</div>
+					<div>
+						&copy; Matt Lamb {new Date().getFullYear()}
+					</div>
 				</section>
 			</footer>
 		</>
