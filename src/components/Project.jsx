b89dfbfc341287098d09f4df2d38a26662dc1458
Matt <mphlamb@gmail.com>
2024-09-22 14:56:18 +0200

    links added as props

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,35 +1,52 @@
 import { Suspense } from "react";
-import "../App.css";
 import Image from "./Image";
 import ErrorBoundary from "./ErrorBoundary";
 
-function Project({name, description, technologies, imageSource, altText}) {
-
-
-    return <>
-    
-    <div className="project-card">
-							<div className="card-image">
-								<ErrorBoundary>
-									<Suspense>
-										<Image
-											imageSource={imageSource}
-											altText={altText}
-										/>
-									</Suspense>
-								</ErrorBoundary>
-							</div>
-							<div className="card-content">
-								<h3 className="card-title">{name}</h3>
-								<div className="card-description">
-									<p>{description}</p>
-									<p>
-										{technologies}
-									</p>
-								</div>
-							</div>
-						</div>
-    </>
+function Project({
+	name,
+	description,
+	technologies,
+	imageSource,
+	altText,
+	github,
+	deployed,
+}) {
+	return (
+		<>
+			<div className="project-card">
+				<div className="project-links-container">
+					<a href={github} target="_blank" rel="noopener noreferrer" className="project-links link-hover">
+						Github ↗
+					</a>
+					<a
+						href={deployed}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="project-links link-hover"
+					>
+						Deployed ↗
+					</a>
+				</div>
+				<div className="card-image">
+					<ErrorBoundary>
+						<Suspense>
+							<Image
+								imageSource={imageSource}
+								altText={altText}
+							/>
+						</Suspense>
+					</ErrorBoundary>
+				</div>
+				<div className="card-content">
+					<h3 className="card-title">{name}</h3>
+					<div className="card-description">
+						<p>{description}</p>
+						<p>{technologies}</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Project;
