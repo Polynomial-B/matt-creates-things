ab10fd505cd62d24f1e0a76706d8ddbb06879b14
Matt <mphlamb@gmail.com>
2024-11-13 15:55:22 +0100

    updates

--- src/components/Titles.jsx
+++ src/components/Titles.jsx
@@ -1,36 +1,40 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState, useRef } from "react";
 
 const titlesArray = [
-    ['JavaScript', ' developer'],
-    ['React.js', ' developer'],
-    ['Full-stack' , ' engineer'],
-    ['App', ' designer'],
-    ['Sound', ' designer'],
-    ['Python', 'developer'],
-]
+	["JavaScript", " developer"],
+	["React.js", " developer"],
+	["Full-stack", " engineer"],
+	["App", " designer"],
+	["Sound", " designer"],
+];
 
 function Titles() {
+	const [jobItem, setJobItem] = useState(titlesArray[0]);
+	const intervalIdRef = useRef(null);
+	const currentIndexRef = useRef(0);
 
-    const [jobItem, setJobItem] = useState(titlesArray[0]);
-    const intervalIdRef = useRef(null);
-    const currentIndexRef = useRef(0);
+	useEffect(() => {
+		const updateJobItem = () => {
+			currentIndexRef.current =
+				(currentIndexRef.current + 1) % titlesArray.length;
+			setJobItem(titlesArray[currentIndexRef.current]);
+		};
 
-    useEffect(() => {
-        const updateJobItem = () => {
-          currentIndexRef.current = (currentIndexRef.current + 1) % titlesArray.length;
-          setJobItem(
-            titlesArray[currentIndexRef.current]);
-        };
+		intervalIdRef.current = setInterval(updateJobItem, 2000);
 
-        intervalIdRef.current = setInterval(updateJobItem, 2000);
+		return () => {
+			clearInterval(intervalIdRef.current);
+		};
+	}, []);
 
-        return () => {
-          clearInterval(intervalIdRef.current);
-        };
-      }, []);
-
-    return <h2 id="job-title"><span id="job-item-one" className="fade-in-out">{jobItem[0]}</span><span id="job-item-two">{jobItem[1]}</span></h2>
-
+	return (
+		<h2 id="job-title">
+			<span id="job-item-one" className="fade-in-out">
+				{jobItem[0]}
+			</span>
+			<span id="job-item-two">{jobItem[1]}</span>
+		</h2>
+	);
 }
 
 export default Titles;
