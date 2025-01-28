ef664f171ac9aed7cde098d6dec0cd9b7ce00bd0
Matt <mphlamb@gmail.com>
2025-01-28 12:30:45 +0100

    first commit

---
+++ src/assets/constants.js
@@ -0,0 +1,55 @@
+export const projects = [
+	{
+		name: "Weather App",
+		description:
+			"A 12-hour weather forecast tool using OpenWeather's APIs.",
+		technologies: "React.js, TypeScript, Tailwind",
+		imageSource: "/images/weather-app.png",
+		altText:
+			"A weather forecast with a pink/purple background, showing description, an image and wind speed and direction.",
+		github: "https://github.com/Polynomial-B/tsx-weather-api",
+		deployed: "",
+	},
+	{
+		name: "SynthSounds",
+		description:
+			"A CRUD app where you can adjust tones, effects and save a synthesiser to your collection.",
+		technologies: "React.js, Django, PostgreSQL, Tone.js, Heroku, CSS",
+		imageSource: "/images/synth-screenshot.png",
+		altText:
+			"A musical synthesiser with each key as a small square in a grid.",
+		github: "https://github.com/Polynomial-B/synth-app-frontend",
+		deployed: "https://synth-sounds.netlify.app/synth",
+	},
+	{
+		name: "Shirtify",
+		description:
+			"A CRUD app where you can customise shirts and save them to your wishlist.",
+		technologies: "MongoDB, Express.js, React.js, Node.js, CSS",
+		imageSource: "/images/shirtify-home.png",
+		altText:
+			"Large red text 'Shirtify', man in green t-shirt and hat, smiling.",
+		github: "https://github.com/Polynomial-B/shirtify-mern-frontend",
+		deployed: "https://shirtify-wishlist.netlify.app/",
+	},
+	{
+		name: "MycoProject",
+		description: "A CRUD app for mushroom identification.",
+		technologies: "MongoDB, Express.js, Node.js, EJS, CSS",
+		imageSource: "/images/myco-home.webp",
+		altText:
+			"Colourful cartoon background. Grid showing 8 mushroom species, some with toxic symbols.",
+		github: "https://github.com/Polynomial-B/myco-project",
+		deployed: "https://myco-project.netlify.app/",
+	},
+	{
+		name: "Cosmic Ray",
+		description: "A game with collision logic and a win scenario.",
+		technologies: "JavaScript, HTML, CSS",
+		imageSource: "/images/cosmic-ray-screenshot.png",
+		altText:
+			"Space-themed background with moving planets, asteroids and a manta ray, in an retro game machine style.",
+		github: "https://github.com/Polynomial-B/cosmic-ray-game",
+		deployed: "https://polynomial-b.github.io/cosmic-ray-game/",
+	},
+];
