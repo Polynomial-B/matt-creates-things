5c799d0e34c51aed8464feb4864be7d6b23bf56e
Matt <mphlamb@gmail.com>
2025-01-28 12:31:42 +0100

    grid for project select added

---
+++ src/components/ProjectGrid.jsx
@@ -0,0 +1,19 @@
+import { projects } from "../assets/constants";
+
+export default function ProjectGrid({ onProjectChange }) {
+	const handleClick = (project) => {
+		onProjectChange(project);
+	};
+
+	return (
+		<ul className="project__grid">
+			{projects.map((project) => {
+				return (
+					<li key={project.name} onClick={() => handleClick(project)}>
+						<img src={project.imageSource} />
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
