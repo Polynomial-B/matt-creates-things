9a8371dc96d04d08e03f14145232faf6402e08ef
Matt <mphlamb@gmail.com>
2025-02-06 10:16:36 +0100

    icon update

--- src/components/Darklight.jsx
+++ src/components/Darklight.jsx
@@ -1,5 +1,7 @@
 import "../Darklight.css";
 import { useState } from "react";
+import IconLight from "./IconLight";
+import IconDark from "./IconDark";
 
 function Darklight() {
 	const getTheme = () => {
@@ -13,8 +15,9 @@ function Darklight() {
 	const [isDark, setIsDark] = useState(getTheme);
 
 	function handleClick() {
-		setIsDark(previousState => {
-			const newState = previousState === "dark-mode" ? "light-mode" : "dark-mode";
+		setIsDark((previousState) => {
+			const newState =
+				previousState === "dark-mode" ? "light-mode" : "dark-mode";
 			if (newState === "light-mode") {
 				document.body.classList.remove("dark-mode");
 				document.body.classList.add("light-mode");
@@ -22,15 +25,24 @@ function Darklight() {
 				document.body.classList.remove("light-mode");
 				document.body.classList.add("dark-mode");
 			}
-	
+
 			return newState;
 		});
 	}
 
 	return (
 		<>
-				{/* <button>{isEnglish}</button> */}
-				<button onClick={handleClick} id={isDark} className="link-hover" title={`Activate ${isDark === "dark-mode" ? "light-mode" : "dark-mode"}`}>{isDark === "dark-mode" ? "lightmode" : "darkmode" }</button>
+			{/* <button>{isEnglish}</button> */}
+			<button
+				onClick={handleClick}
+				id={isDark}
+				className="link-hover"
+				title={`Activate ${
+					isDark === "dark-mode" ? "light-mode" : "dark-mode"
+				}`}
+			>
+				{isDark === "dark-mode" ? <IconLight /> : <IconDark />}
+			</button>
 		</>
 	);
 }
--- src/components/Titles.jsx
+++ src/components/Titles.jsx
@@ -1,38 +1,12 @@
-import { useEffect, useState, useRef } from "react";
-
-const titlesArray = [
-	["JavaScript", " developer"],
-	["React.js", " developer"],
-	["Full-stack", " engineer"],
-	["App", " designer"],
-	["Sound", " designer"],
-];
-
 function Titles() {
-	const [jobItem, setJobItem] = useState(titlesArray[0]);
-	const intervalIdRef = useRef(null);
-	const currentIndexRef = useRef(0);
-
-	useEffect(() => {
-		const updateJobItem = () => {
-			currentIndexRef.current =
-				(currentIndexRef.current + 1) % titlesArray.length;
-			setJobItem(titlesArray[currentIndexRef.current]);
-		};
-
-		intervalIdRef.current = setInterval(updateJobItem, 2000);
-
-		return () => {
-			clearInterval(intervalIdRef.current);
-		};
-	}, []);
-
 	return (
 		<h2 id="job-title">
-			<span id="job-item-one" className="fade-in-out">
-				{jobItem[0]}
+			<span id="job-item-one" className="fade-in">
+				Full-stack
 			</span>
-			<span id="job-item-two">{jobItem[1]}</span>
+			<span id="job-item-two" className="fade-in">
+				developer
+			</span>
 		</h2>
 	);
 }
