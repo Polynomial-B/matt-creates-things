b4f04c736e0aca273dce98d452fef00980e4486c
Matt <mphlamb@gmail.com>
2025-02-12 18:44:51 +0100

    end image added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,7 @@ import Experience from "./Experience";
 import Currently from "./Interests";
 import ProjectGrid from "./ProjectGrid";
 import { projects } from "../assets/constants";
+import { EndBreak } from "./EndBreak";
 
 function Home() {
 	const [activeProject, setActiveProject] = useState(projects[0]);
@@ -19,6 +20,32 @@ function Home() {
 	const handleSelectProject = (project) => {
 		setActiveProject(project);
 	};
+
+	const getTheme = () => {
+		const prefersDark = window.matchMedia(
+			"(prefers-color-scheme: dark)"
+		).matches;
+
+		return prefersDark ? "dark-mode" : "light-mode";
+	};
+
+	const [isDark, setIsDark] = useState(getTheme);
+
+	function handleIsDark() {
+		setIsDark((previousState) => {
+			const newState =
+				previousState === "dark-mode" ? "light-mode" : "dark-mode";
+			if (newState === "light-mode") {
+				document.body.classList.remove("dark-mode");
+				document.body.classList.add("light-mode");
+			} else {
+				document.body.classList.remove("light-mode");
+				document.body.classList.add("dark-mode");
+			}
+
+			return newState;
+		});
+	}
 
 	return (
 		<>
@@ -53,7 +80,7 @@ function Home() {
 				>
 					contact
 				</button>
-				<Darklight />
+				<Darklight isDark={isDark} handleIsDark={handleIsDark} />
 			</nav>
 			<header id="splash">
 				<div id="splash-container">
@@ -240,68 +267,61 @@ function Home() {
 				</section>
 			</main>
 			<footer>
-				<section className="contact" ref={contactRef}>
-					<h2 id="contact">Contact</h2>
-					<div id="footer-email">m.lamb.dev@gmail.com</div>
-					<ul>
-						<li className="contact-link">
-							<a
-								target="_blank"
-								href="http://www.linkedin.com/in/matt-lamb-dev"
-								rel="noopener noreferrer"
-							>
-								<figure>
-									<figcaption>LinkedIn</figcaption>
-									<i className="devicon-linkedin-plain" />
-								</figure>
-							</a>
-						</li>
-						<li className="contact-link">
-							<a
-								target="_blank"
-								href="https://github.com/Polynomial-B"
-								rel="noopener noreferrer"
-							>
-								<figure>
-									<figcaption>GitHub</figcaption>
-									<i className="devicon-github-original" />
-								</figure>
-							</a>
-						</li>
-						<li className="contact-link">
-							<a
-								target="_blank"
-								href="mailto:m.lamb.dev@gmail.com"
-							>
-								<figure>
-									<figcaption>E-mail</figcaption>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 448 512"
-									>
-										<path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM218 271.7L64.2 172.4C66 156.4 79.5 144 96 144l256 0c16.5 0 30 12.4 31.8 28.4L230 271.7c-1.8 1.2-3.9 1.8-6 1.8s-4.2-.6-6-1.8zm29.4 26.9L384 210.4 384 336c0 17.7-14.3 32-32 32L96 368c-17.7 0-32-14.3-32-32l0-125.6 136.6 88.2c7 4.5 15.1 6.9 23.4 6.9s16.4-2.4 23.4-6.9z" />
-									</svg>
-								</figure>
-							</a>
-						</li>
-					</ul>
-				</section>
-
-				<section className="legal">
-					{/* <button
-						className="scroll-to-top link-hover"
-						onClick={() =>
-							homeRef.current?.scrollIntoView({
-								behavior: "smooth",
-							})
-						}
-					>
-						<span>scroll to top</span>
-					</button> */}
-					<div id="copyright">
-						&copy; Matt Lamb {new Date().getFullYear()}
+				<div className="footer-container">
+					<section className="contact" ref={contactRef}>
+						<h2 id="contact">Contact</h2>
+						<div id="footer-email">m.lamb.dev@gmail.com</div>
+						<ul>
+							<li className="contact-link">
+								<a
+									target="_blank"
+									href="http://www.linkedin.com/in/matt-lamb-dev"
+									rel="noopener noreferrer"
+								>
+									<figure>
+										<figcaption>LinkedIn</figcaption>
+										<i className="devicon-linkedin-plain" />
+									</figure>
+								</a>
+							</li>
+							<li className="contact-link">
+								<a
+									target="_blank"
+									href="https://github.com/Polynomial-B"
+									rel="noopener noreferrer"
+								>
+									<figure>
+										<figcaption>GitHub</figcaption>
+										<i className="devicon-github-original" />
+									</figure>
+								</a>
+							</li>
+							<li className="contact-link">
+								<a
+									target="_blank"
+									href="mailto:m.lamb.dev@gmail.com"
+								>
+									<figure>
+										<figcaption>E-mail</figcaption>
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 448 512"
+										>
+											<path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM218 271.7L64.2 172.4C66 156.4 79.5 144 96 144l256 0c16.5 0 30 12.4 31.8 28.4L230 271.7c-1.8 1.2-3.9 1.8-6 1.8s-4.2-.6-6-1.8zm29.4 26.9L384 210.4 384 336c0 17.7-14.3 32-32 32L96 368c-17.7 0-32-14.3-32-32l0-125.6 136.6 88.2c7 4.5 15.1 6.9 23.4 6.9s16.4-2.4 23.4-6.9z" />
+										</svg>
+									</figure>
+								</a>
+							</li>
+						</ul>
+						<div id="copyright">
+							&copy; Matt Lamb {new Date().getFullYear()}
+						</div>
+						<div></div>
+					</section>
+					<div className="end">
+						<EndBreak isDark={isDark} />
 					</div>
-				</section>
+				</div>
 			</footer>
 		</>
 	);
