2ad5ea94dd02a9ca51a135dc15719e69e1c1915b
Matt <mphlamb@gmail.com>
2025-02-18 17:23:21 +0100

    text updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -180,31 +180,31 @@ function Home() {
 					<ul className="experience">
 						<li>
 							<Experience
-								title="Sofware Engineering Student, General Assembly"
+								title="Software Engineering Student, General Assembly"
 								dates="2024"
-								list1="Learned full-stack technologies using SQL and NoSQL databases."
-								list2="Contributed to daily standup, live classes and practical labs."
-								list3="Worked on solo and paired projects within short timeframes."
-								list4="Learned debugging, code refactoring and problem-solving."
+								list1="Learned full-stack technologies using SQL and NoSQL databases"
+								list2="Contributed to daily standup, live classes and practical labs"
+								list3="Worked on solo and paired projects within short timeframes"
+								list4="Learned debugging, code refactoring and problem-solving"
 							/>
 						</li>
 						<li>
 							<Experience
 								title="Private English Tutor"
 								dates="2022 - present"
-								list1="Managed personal brand: communication, marketing and self-promotion."
-								list2="Created content, materials and curriculum: provided targets and activities."
-								list3="Organised game events, workshops and outings."
+								list1="Managed personal brand: communication, marketing and self-promotion"
+								list2="Created content, materials and curriculum: provided targets and activities"
+								list3="Organised events: games, workshops and outings"
 							/>
 						</li>
 						<li>
 							<Experience
 								title="Lifeboat Operations Coordinator, RNLI"
 								dates="2020 - 2022"
-								list1="Provided on-call support for lifeboats and lifeguards."
-								list2="Trained and on-boarded staff, both in-person and remote."
-								list3="Cleaned data and updated staff intranet."
-								list4="Distributed physical and digital maps."
+								list1="Provided on-call support for lifeboats and lifeguards"
+								list2="Trained and on-boarded staff, both in-person and remote"
+								list3="Cleaned data and updated staff intranet"
+								list4="Distributed physical and digital maps"
 							/>
 						</li>
 					</ul>
