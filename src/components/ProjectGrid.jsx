9cfe58395f8039459f7814d1062ec3745d2d1d68
Matt <mphlamb@gmail.com>
2025-02-18 13:19:11 +0100

    updates

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import { projects } from "../assets/constants";
+import ErrorBoundary from "./ErrorBoundary";
 
 export default function ProjectGrid({ onProjectChange }) {
 	const handleClick = (project) => {
@@ -7,11 +9,23 @@ export default function ProjectGrid({ onProjectChange }) {
 
 	return (
 		<ul className="project__grid">
-			{projects.map((project) => {
+			{projects.map((project, index) => {
 				return (
-					<li key={project.name} onClick={() => handleClick(project)}>
-						<img src={project.imageSource} alt={project.altText} />
-					</li>
+					<>
+						<li
+							key={project.name + index}
+							onClick={() => handleClick(project)}
+						>
+							<ErrorBoundary>
+								<Suspense>
+									<img
+										src={project.imageSource}
+										alt={project.altText}
+									/>
+								</Suspense>
+							</ErrorBoundary>
+						</li>
+					</>
 				);
 			})}
 		</ul>
